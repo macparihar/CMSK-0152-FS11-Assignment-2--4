6b7d874bdc49a2fe2596b095e89614f41a50b3d7
macparihar <156152583+macparihar@users.noreply.github.com>
2024-02-29 17:21:43 -0700

    done

---
+++ Assignment 4/task1.js
@@ -0,0 +1,9 @@
+// Task 1
+
+for (let index = 0; index <= 200; index++) {
+    if (index % 2 == 0) {
+        console.log(index + " is even.");
+    } else {
+        console.log(index + " is odd.");
+    }
+}
